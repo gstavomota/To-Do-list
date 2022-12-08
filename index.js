17418c35d1d633d45e5e7765aad320e6dcef4f67
Gustavo <gstavomota1@gmail.com>
2022-12-08 19:36:24 -0300

    feat: criando arquitetura de pastas

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const porta = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World');
 })
-app.listen(3000)
+
+app.listen(porta, () =>{
+    console.log(`Servidor rodando na porta ${porta}`)
+});
