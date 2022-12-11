0e95597b332bf0bb82740208e0add81ed2ba8c9b
Gustavo <gstavomota1@gmail.com>
2022-12-10 23:23:10 -0300

    feat: adicionando editar e apagar

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,8 @@ const TaskController = require('../controller/TaskController');
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
-routes.get("/getById", TaskController.getbyId);
+routes.get("/getById/:id/:method", TaskController.getbyId);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleOneTask);
 
 module.exports = routes;
